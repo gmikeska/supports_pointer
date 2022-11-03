3a67d6b9ef2c38b9abafd1a1eabb4b6a88679366
Greg Mikeska <gmikeska07@gmail.com>
2022-11-03 14:47:20 -0500

    update dummy app and get rspec working

--- spec/dummy/app/models/blog_post.rb
+++ spec/dummy/app/models/blog_post.rb
@@ -1,7 +1,7 @@
 class BlogPost < ApplicationRecord
   include SupportsPointer
 
-  parses_pointer :blogpost, Regexp::Template.new(atoms:[:"^",:"(?<slug>\w*)"])
+  parses_pointer :blogpost, template:Regexp::Template.new(atoms:[:"\^",:"(?<slug>\w*)"])
 
   pointer_resolution :blogpost do |data|
     BlogPost.find_by slug:data[:slug]
--- spec/dummy/spec/rails_helper.rb
+++ spec/dummy/spec/rails_helper.rb
@@ -6,8 +6,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'rspec/rails'
-require 'devise'
-require_relative 'support/controller_macros'
+# require_relative 'support/controller_macros'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -62,8 +61,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include DeviseRequestSpecHelpers, :type => :request
-  config.include Warden::Test::Helpers
+  # config.include DeviseRequestSpecHelpers, :type => :request
+  # config.include Warden::Test::Helpers
   config.include FactoryBot::Syntax::Methods
-  config.include ControllerMacros, :type => :request
+  # config.include ControllerMacros, :type => :request
 end
